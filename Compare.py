a0fb6092cd6b0a46e42878a08e396d388ff8d71f
ta2bss <ta2bss@yahoo.com>
2022-04-17 21:49:40 +0300

    2022-04-15 BeautifulSoup fixed

--- Compare.py
+++ Compare.py
@@ -1,6 +1,6 @@
 import requests
 import os
-url = 'http://milliyet.com/'
+url = 'https://api.nodes.guru/aptos_update.sh'
 r = requests.get(url, allow_redirects=True)
 f= open ("data","wb")
 f.write(r.content)
