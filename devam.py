7a8a5f5824a5e2bec5232161b3f63e6d5383d0d3
ta2bss <ta2bss@yahoo.com>
2022-04-18 11:30:58 +0300

    2022-04-15 BeautifulSoup fixed

---
+++ devam.py
@@ -0,0 +1,22 @@
+import hashlib
+from bs4 import BeautifulSoup as bs
+import requests_html
+from datetime import datetime
+
+simdi = datetime.now()
+su_an = simdi.strftime("%Y-%m-%d %H:%M")
+
+
+# .htm , .html , .shtml , .asp , .pl , .cgi , .jsp. , php
+s = requests_html.HTMLSession()
+target = ["https://nodes.guru/"  , "https://nodes.guru/subspace/setup-guide/en" ,"https://www.milliyet.com.tr/"]
+for x in (range(len(target))):
+    page = s.get(target[x])
+    soup=bs(page.text,'lxml')
+    contents = soup.get_text()
+    hash_object = hashlib.md5(contents.encode())
+    print(hash_object.hexdigest())
+
+    f=open ("data","a")
+    f.write(target[x] +" --> "+ su_an+" --> "+ hash_object.hexdigest()+"\n")
+    f.close()
