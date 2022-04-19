7e440470c84ec98e56e2c04178cf896e0fc476ee
ta2bss <ta2bss@yahoo.com>
2022-04-19 21:06:08 +0300

    Devam

--- devam4.py
+++ devam4.py
@@ -4,38 +4,37 @@ from bs4 import BeautifulSoup as bs
 import requests_html
 from datetime import datetime
 
-
-
 now = datetime.now()
 date_time = now.strftime("%Y-%m-%d %H:%M")
-
 
 # .htm , .html , .shtml , .asp , .pl , .cgi , .jsp. , php
 
 s = requests_html.HTMLSession()
-target = ["https://nodes.guru/"  , "https://nodes.guru/subspace/setup-guide/en", "https://nodes.guru/aptos/setup-guide/en"]
+target = ["https://nodes.guru"  , "https://nodes.guru/subspace/setup-guide/en", "https://nodes.guru/aptos/setup-guide/en" ,"https://stackoverflow.com/" ]
 
 for x in (range(len(target))):
     page = s.get(target[x])
     soup=bs(page.text,'lxml')
     contents = soup.get_text()
 
-
-
-
+    oldpage = target[x]
+    oldpage = oldpage.replace(":","").replace("/","").replace("https","").replace("http","")
     hash_object = hashlib.md5(contents.encode())
-    print(hash_object.hexdigest())
 
     f=open ("data","a")
     f.write(target[x] +" --> "+ date_time+" --> "+ hash_object.hexdigest()+"\n")
     f.close()
 
-    oldpage = target[x]
-    oldpage = oldpage.replace(":","").replace("/","")
-    print (oldpage)
-
-
-
     f=open (oldpage +".dat","w")
     f.write(contents+"\n"+hash_object.hexdigest())
     f.close()
+
+
+
+f=open ("data","r")
+d = open ("datasorted","w")
+for line in sorted(f):
+    d.write(line)
+f.close()
+d.close()
+
