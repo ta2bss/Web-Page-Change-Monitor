1dbced7cd49413995b798c389d84703f8ff034c7
ta2bss <ta2bss@yahoo.com>
2022-04-20 20:58:28 +0300

    variables renamed

--- downloads/recognisable-name/nodes.guru/_next/static/pjU7uzHWXjXT5Wd3Mg96a/964e032b___ssgManifest.js
+++
@@ -1,7 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F404","\u002Fdonate","\u002Fnot-found","\u002F","\u002Fnym\u002Fmixnodecheck"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
-/*!
-* AerWebCopy Engine [version 6.3.0]
-* Copyright Aeroson Systems & Co.
-* File mirrored from https://nodes.guru/_next/static/pjU7uzHWXjXT5Wd3Mg96a/_ssgManifest.js
-* At UTC time: 2022-04-18 15:58:23.206079
-*/
