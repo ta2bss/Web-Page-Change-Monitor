69366be446f9c633eaef6f151e620fec6b7481a4
ta2bss <ta2bss@yahoo.com>
2022-04-20 14:26:18 +0300

    Folderlar düzenlendi Şimdi folder içinde file extensions rename için çalışılacak

--- devam4.py
+++ devam4.py
@@ -3,6 +3,19 @@ import hashlib
 from bs4 import BeautifulSoup as bs
 import requests_html
 from datetime import datetime
+import os
+
+pathDatas = "Datas"
+pathPages = "Datas1\Pages"
+
+try:
+    os.mkdir(pathDatas)
+except:
+    pass
+try:
+    os.mkdir(pathPages)
+except:
+    pass
 
 now = datetime.now()
 date_time = now.strftime("%Y-%m-%d %H:%M")
@@ -10,7 +23,7 @@ date_time = now.strftime("%Y-%m-%d %H:%M")
 #FOR  .htm , .html , .shtml , .asp , .pl , .cgi , .jsp. , php PAGES
 s = requests_html.HTMLSession()
 
-target = ["https://nodes.guru"  , "https://nodes.guru/subspace/setup-guide/en", "https://nodes.guru/aptos/setup-guide/en" ,"https://stackoverflow.com/" , "http://ta2bss.com/" ]
+target = ["https://nodes.guru"  , "https://nodes.guru/subspace/setup-guide/en", "https://nodes.guru/aptos/setup-guide/en" ,"https://stackoverflow.com/"  ]
 
 for x in (range(len(target))):
     page = s.get(target[x])
@@ -21,19 +34,19 @@ for x in (range(len(target))):
     pagename = pagename.replace(":","").replace("/","").replace("https","").replace("http","")
     hash_object = hashlib.md5(encodedcontents)
 
-    f=open ("Datas\\data","a")
+    f=open ("Datas\\data", "a")
     f.write(target[x] +" --> "+ date_time+" --> "+ hash_object.hexdigest()+"\n")
     f.close()
     stringencodedcontents = str (encodedcontents)
     stringencodedcontents = stringencodedcontents.replace("\\n"," ").replace("\\t"," ")
-    f=open ("Datas\\"+pagename +".dat","w")
+    f=open ("Datas\\Pages\\"+pagename +".new","w")
     f.write("Date-Time:"+date_time+"\nPageHash:===>"+hash_object.hexdigest()+"<==="+"\n"+stringencodedcontents)
     f.close()
 
 
 
-f=open ("Datas\\data","r")
-d = open ("Datas\\datasorted","w")
+f=open ("Datas\\data", "r")
+d = open ("Datas\\datasorted", "w")
 for line in sorted(f):
     d.write(line)
 f.close()
